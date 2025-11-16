650453acda2c7c054c7d372a016b531ef86d53d1
ki8yk8 <ki***@gmail.com>
2025-11-16 21:02:58 +0545

    adds tests for color component of engine

--- src/tests/colors.test.js
+++ src/tests/colors.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { rgbToHex, hexToRgb } from "../blip";
+import { rgbToHex, hexToRgb, color } from "../blip";
 
 describe("Colors Uilities Test", () => {
 	test("Hex to RGB should work for 6-string format", () => {
@@ -8,5 +8,15 @@ describe("Colors Uilities Test", () => {
 
 	test("RGB to Hex must work", () => {
 		expect(rgbToHex(255, 255, 255)).toBe("#ffffff");
-	})
+	});
+
+	test("color should accept hex value", () => {
+		expect(color("#fffff")).toEqual({ r: 255, g: 255, b: 255 });
+	});
+	test("color should accept rgb value in list", () => {
+		expect(color(255, 255, 255)).toBe({ r: 255, g: 255, b: 255 });
+	});
+	test("color should accept rgb value in object with keys r, g, b", () => {
+		expect(color({ r: 255, g: 255, b: 255 })).toBe({ r: 255, g: 255, b: 255 });
+	});
 });
