a4f47a2f50feab6daf7dcef62434f57806f374b1
ki8yk8 <ki***@gmail.com>
2025-11-17 14:28:44 +0545

    creates tests framework for vectors

---
+++ src/tests/vec2.test.js
@@ -0,0 +1,32 @@
+import { describe, expect, test } from "vitest";
+
+describe("Vec2 should be initialized in different conditions", () => {
+	test.todo("While passing objects");
+	test.todo("While passing an array");
+	test.todo("While passing two numbers");
+	test.todo("While passing same instance");
+});
+
+describe("Vec2 should have working arithmetic function", () => {
+	test.todo("Addition");
+	test.todo("Substraction");
+	test.todo("Scaling");
+	test.todo("Scaling x, y independently");
+	test.todo("Negation");
+	test.todo("Zero check");
+	test.todo("Equals")
+})
+
+describe("Vec2 should implement vector functions", () => {
+	test.todo("Distance compute");
+	test.todo("Squared distance compute");
+	test.todo("Compute length");
+	test.todo("Unit vector");
+	test.todo("Dot product");
+	test.todo("Cross vector");
+	test.todo("Angle");
+	test.todo("Angle between");
+	test.todo("rotation");
+})
+
+
