2ab56926e5c8a0c0da78264fe201c98d2c569a89
ki8yk8 <ki***@gmail.com>
2025-11-17 14:03:43 +0545

    creates framework for implementing vec2

---
+++ src/engine/vec2.js
@@ -0,0 +1,23 @@
+class Vec2{
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	clone() {
+		return Vec2(this.x, this.y);
+	}
+	add() {}
+	sub(){}
+	scale(){}
+	dist(){}
+	sdist(){}
+	unit(){}
+	dot(){}
+	cross(){}
+	angle(){}
+	angleBetween(){}
+	isZero(){}
+	eq(){}
+	rotate(){}
+}
