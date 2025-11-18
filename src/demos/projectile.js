441e582a3adbb8dba0921503387d0cb4ccb0f4ee
ki8yk8 <ki***@gmail.com>
2025-11-18 22:03:47 +0545

    adds key presses to change the angle

--- src/demos/projectile.js
+++ src/demos/projectile.js
@@ -15,9 +15,6 @@ const ground = k.add([
 	k.tag("ground"),
 ]);
 
-// acceleration due to gravity in pixels per second square
-const g = 200;
-
 const press_up_hint = k.add([
 	k.text("Press arrow up key to move the cannon upward"),
 	k.pos(k.width() / 2, 100),
@@ -30,3 +27,31 @@ const press_space_hint = k.add([
 	k.text("Press space to shoot"),
 	k.pos(k.width() / 2, press_down_hint.pos.y + press_down_hint.height + 10),
 ]);
+
+// acceleration due to gravity in pixels per second square
+const g = 200;
+
+const ground_top = ground.pos.y - ground.height;
+const ball = k.add([
+	k.rect(50, 50, { radius: 25 }),
+	k.pos(100, ground_top - 25),
+	k.area("ball"),
+]);
+
+const ruler = k.add([
+	k.rect(100, 10),
+	k.pos(ball.pos.x, ball.pos.y),
+	k.anchor("left"),
+	k.color("#20bf55")
+])
+
+k.onKeyPressed("ArrowUp", () => {
+	let new_angle = ruler.angle - 10;
+	new_angle = k.clamp(new_angle, -90, 0);
+	ruler.angle = new_angle;
+})
+k.onKeyPressed("ArrowDown", () => {
+	let new_angle = ruler.angle + 10;
+	new_angle = k.clamp(new_angle, -90, 0);
+	ruler.angle = new_angle;
+})
