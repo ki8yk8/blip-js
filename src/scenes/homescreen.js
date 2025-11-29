9aadfe9f5d7562fa8f27c3b443e57f2abebb0cb5
ki8yk8 <ki***@gmail.com>
2025-11-29 23:09:24 +0545

    creates menu item for the navigation

--- src/scenes/homescreen.js
+++ src/scenes/homescreen.js
@@ -20,5 +20,19 @@ export function registerHomeScreen({ k, state, constants }) {
 			[logo.pos, logo.pos.add(0, -20), logo.pos, logo.pos.add(0, 20), logo.pos],
 			8
 		);
+
+		// adding the menu buttons
+		const menu_items = ["Start Game", "Instruction"];
+		menu_items.forEach((item, index) => {
+			// rectangle background
+			const background = k.add([
+				k.rect(500, 80),
+				k.pos(k.width() / 2, logo.pos.y + 150 + 100 * index + 8 * index),
+				k.color("SKYBLUE"),
+			]);
+
+			// rectangle text
+			const text = k.add([k.text(item, { size: 48 }), k.pos(background.pos)]);
+		});
 	});
 }
