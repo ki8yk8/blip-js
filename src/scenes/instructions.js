a0d6e2e62ef3ce571f10958b26e960adf6e63b31
ki8yk8 <ki***@gmail.com>
2025-11-29 23:46:53 +0545

    adds layout for instructions scene

--- src/scenes/instructions.js
+++ src/scenes/instructions.js
@@ -1,5 +1,11 @@
 export function registerInstructionsScreen({ k }) {
 	k.scene("instructions", () => {
+		const logo = k.add([
+			k.text("Instructions", { size: 48 }),
+			k.pos(k.width() / 2, 100),
+			k.color("PURPLE"),
+		]);
+
 		const hint = k.add([
 			k.text("Press space to continue"),
 			k.pos(k.width() / 2, k.height() - 100),
@@ -8,6 +14,27 @@ export function registerInstructionsScreen({ k }) {
 			k.color("BROWN"),
 		]);
 
+		const instructions = ["Instruction 1", "Instruction 2", "Instruction 3"];
+		let index = 0;
+
+		const instruction_objects = k.add([
+			k.text(instructions[index], {
+				size: 24,
+				maxWidth: k.min(k.width() - 50, 400),
+				align: "center",
+			}),
+			k.pos(k.width() / 2, logo.pos.y + logo.height / 2 + 50),
+			k.color("BLACK"),
+		]);
+
+		const pagination = k.add([
+			k.text(`Page: ${index + 1}/${instructions.length}`),
+			k.pos(k.width() - 20, 100),
+			k.anchor("right"),
+			k.color("GREEN"),
+		]);
+
+		// animating the objects
 		k.animate(hint, "angle", [0, 2, 0, -2, 0], 4);
 		k.animate(
 			hint,
