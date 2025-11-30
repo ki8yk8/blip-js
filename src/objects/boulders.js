b335dade0bb4ce9d8ab69d537268de041c7e7011
ki8yk8 <ki***@gmail.com>
2025-12-01 02:38:30 +0545

    makes the boulder rectangle to avoid conufsion due to rounded corners during collision

--- src/objects/boulders.js
+++ src/objects/boulders.js
@@ -2,7 +2,8 @@ import { get_random_pos } from "../utils/pos";
 
 export default function Boulder({ k, pos }) {
 	const boulder = k.add([
-		k.rect(100, 100, { radius: [40, 30, 50, 20] }),
+		// k.rect(100, 100, { radius: [40, 30, 50, 20] }),
+		k.rect(100, 100, { radius: 8 }),
 		k.pos(pos),
 		k.color("#243A56"),
 		k.area(),
@@ -10,8 +11,8 @@ export default function Boulder({ k, pos }) {
 	]);
 
 	k.add([
-		k.rect(20, 50, { radius: [0, 50, 10, 4] }),
-		k.pos(boulder.pos.add(boulder.width / 2 - 16, -12)),
+		k.rect(20, 50, { radius: [0, 8, 0, 50] }),
+		k.pos(boulder.pos.add(boulder.width / 2 -18 , -18)),
 		k.color("#4276AF"),
 	]);
 
