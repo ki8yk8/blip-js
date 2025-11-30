9e59cb0df6e4a9904bebb8587086e0461df2e6c8
ki8yk8 <ki***@gmail.com>
2025-11-30 12:20:16 +0545

    creates progress bar logic

--- src/scenes/loader.js
+++ src/scenes/loader.js
@@ -10,5 +10,38 @@ export function registerLoaderScene({ k, constants, state }) {
 			steel: "/sprites/steel.png",
 		};
 
+		const progress_bar_bg = k.add([
+			k.rect(400, 80),
+			k.color("BLACK"),
+			k.pos(k.width() / 2, k.height() / 2),
+		]);
+		const progress_bar = k.add([
+			k.rect(10, 80 - 12),
+			k.pos(progress_bar_bg.pos.add(-progress_bar_bg.width / 2 + 6, 0)),
+			k.anchor("left"),
+		]);
+
+		let loaded = 0;
+		const total = Object.keys(to_load_sprites).length;
+
+		// 0 => 10, 100 => progress_bar_bg.width-12
+		function increaseProgress() {
+			const width = k.map(
+				(loaded / total) * 100,
+				0,
+				10,
+				100,
+				progress_bar_bg.width - 12
+			);
+			progress_bar.width = width;
+			progress_bar.rect.width = width;
+		}
+
+		Object.entries(to_load_sprites).forEach(async ([name, path]) => {
+			await k.wait(1, () => {});
+			await k.loadSprite(name, path);
+			loaded++;
+			increaseProgress();
+		});
 	});
 }
