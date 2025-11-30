8938b6c2eff5ac972c411853825cf96ab66e690c
ki8yk8 <ki***@gmail.com>
2025-11-30 11:14:02 +0545

    adds particle effects in game over scene

--- src/scenes/over.js
+++ src/scenes/over.js
@@ -1,3 +1,5 @@
+const DELAY = 0.8;
+
 export function registerGameOverScene({ k, state, constants }) {
 	k.scene("over", (score) => {
 		const score_title = k.add([
@@ -11,6 +13,34 @@ export function registerGameOverScene({ k, state, constants }) {
 			k.pos(score_title.pos.add(0, 64)),
 			k.color("PURPLE"),
 		]);
+
+		const particle_left = k.add([
+			k.pos(k.width() / 2 - 100, 200),
+			k.particles({
+				opacities: [k.vec2(1.0), k.vec2(0.0)],
+				spread: 45,
+				direction: -135,
+				colors: [k.Color("RED"), k.Color("ORANGE"), k.Color("GREEN")],
+				lifetime: 1,
+				speed: 50,
+			}),
+		]);
+		k.wait(DELAY, () => particle_left.emit(100));
+		k.wait(DELAY + 0.25, () => particle_left.emit(50));
+
+		const particle_right = k.add([
+			k.pos(k.width() / 2 + 100, 200),
+			k.particles({
+				opacities: [k.vec2(1.0), k.vec2(0.0)],
+				spread: 45,
+				direction: -45,
+				colors: [k.Color("RED"), k.Color("ORANGE"), k.Color("GREEN")],
+				lifetime: 1,
+				speed: 50,
+			}),
+		]);
+		k.wait(2, () => particle_right.emit(100));
+		k.wait(DELAY + 0.25, () => particle_right.emit(50));
 
 		const hint = k.add([
 			k.text("Press space to continue"),
