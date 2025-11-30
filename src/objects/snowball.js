b1f61d1f1625a4e971d6edc48c91649590fbbc31
ki8yk8 <ki***@gmail.com>
2025-11-30 19:18:16 +0545

    adds snowball physics logic

--- src/objects/snowball.js
+++ src/objects/snowball.js
@@ -1,10 +1,35 @@
-export default function Snowball({k, constants, state}) {
+export default function Snowball({ k, constants, state }) {
 	const snowball = k.add([
-		k.rect(50, 50, {radius: 25}),
+		k.rect(50, 50, { radius: 25 }),
 		k.anchor("bot"),
-		k.pos(k.width()/2, 100),
+		k.pos(k.width() / 2, 100),
+		k.area(),
 		"snowball",
-	])
+	]);
+
+	snowball.acc = k.vec2(0, 200);
+
+	snowball.onCollide("snowblock", () => {
+		snowball.acc = k.vec2(0, 200);
+		snowball.vel = snowball.vel.scale(0.4).neg();
+	});
+
+	// do not let snowball cross the boundary ever
+	k.onUpdate(() => {
+		snowball.pos.x = k.clamp(
+			snowball.pos.x,
+			snowball.width / 2,
+			k.width() - snowball.width / 2
+		);
+		snowball.pos.y = k.clamp(snowball.pos.y, snowball.height, k.height() - 64);
+	});
+
+	k.onKeyDown(" ", () => {
+		if (snowball.checkCollision("snowblock")) {
+			snowball.acc = k.vec2(0, 200);
+			snowball.vel = snowball.vel.add(0, -200);
+		}
+	});
 
 	return snowball;
 }
